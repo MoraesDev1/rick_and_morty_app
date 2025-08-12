d84f88bdb867613c2120d27a10356f75c372f730
Moraes <moraestech01@gmail.com>
2025-08-12 19:04:01 -0300

    feat: Add additional asset paths for icons representing species, gender, current location, and origin

--- lib/core/enum/assets_paths.dart
+++ lib/core/enum/assets_paths.dart
@@ -2,7 +2,13 @@ enum AssetsPaths {
   loader('assets/images/portal.png'),
   failure('assets/images/morty_failure.png'),
   building('assets/images/building.png'),
-  avatar('assets/images/avatar.png');
+  frame('assets/images/avatar_frame.png'),
+  avatar('assets/images/avatar.png'),
+  speciesIcon('assets/icons/specie_icon.png'),
+  genderIcon('assets/icons/gender_icon.png'),
+  currentLocationIcon('assets/icons/current_location_icon.png'),
+  originIcon('assets/icons/origin_icon.png');
+  
 
   final String path;
 
