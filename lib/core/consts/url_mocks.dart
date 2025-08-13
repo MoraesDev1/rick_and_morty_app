6fa89f063e687d7928a91e15be9c18a067201181
Moraes <moraestech01@gmail.com>
2025-08-13 18:01:25 -0300

    feat: Add default episode URL to UrlMocks class

--- lib/core/consts/url_mocks.dart
+++ lib/core/consts/url_mocks.dart
@@ -1,3 +1,4 @@
 class UrlMocks {
   static const String locationDefault = 'https://wallpapers.com/images/high/supernatural-and-colorful-rick-and-morty-stoner-4bpkp0x162y1jn87.webp';
+  static const String episodeDefault = 'https://image-cdn.hypb.st/https%3A%2F%2Fhypebeast.com%2Fimage%2F2022%2F09%2Frick-and-morty-season-6-episode-1-premiere-opening-stream-tw.jpg?w=960&cbr=1&q=90&fit=max';
 }
